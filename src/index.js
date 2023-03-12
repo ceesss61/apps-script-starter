cc5669211f7af98b3d36b16605b2391e9f42f0a7
ceesss61 <ensar@ceess.com.tr>
2023-03-12 17:58:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import './server/mail';
 import './server/menu';
 import './server/webapp';
-import { makeQueryString } from './http';
 
-global.testimport = () => {
-  const url = makeQueryString('https://google.com');
-  Logger.log(`Query string is ${url}`);
-};
 /* Written by Amit Agarwal */
